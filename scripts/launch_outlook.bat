be2e8f561ae75cdde9eb1298c636cb89891f16dc
Dylan Kinnett <dkinnett@thewalters.org>
2021-10-14 16:46:10 -0400

    Add scripts for tidiness

--- scripts/launch_outlook.bat
+++ scripts/launch_outlook.bat
@@ -1,1 +1,15 @@
-../../launch_outlook.bat
+::-----------------------------------------------------------------------------
+:Launch Outlook and Calendar Sync
+:: author: @dylan-k
+:: See Also: https://github.com/phw198/OutlookGoogleCalendarSync
+::-----------------------------------------------------------------------------
+
+@ECHO OFF
+
+:: if closed, open Outlook
+wmic process where "name='OUTLOOK.EXE'" get ProcessID | find /i "ProcessId" > nul || (START /max "Microsoft Outlook" /D "C:\Program Files\Microsoft Office\root\Office16\" OUTLOOK.EXE /recycle)
+
+:: if closed, open Calendar Sync
+wmic process where "name='OutlookGoogleCalendarSync.exe'" get ProcessID | find /i "ProcessId" > nul || (START "Outlook Google Calendar Sync"  /D "%USERPROFILE%\AppData\Local\OutlookGoogleCalendarSync\" OutlookGoogleCalendarSync.exe)
+
+EXIT 
